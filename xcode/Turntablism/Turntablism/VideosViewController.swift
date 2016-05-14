34e5bf789f2191f8b74f5b7a0fe684a8e9bf42fb
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-05-14 17:08:29 +0200

    Deploy target on iPad Mini

---
+++ xcode/Turntablism/Turntablism/VideosViewController.swift
@@ -0,0 +1,17 @@
+//
+//  VideosViewController.swift
+//  Turntablism
+//
+//  Created by Thibault Maekelbergh on 14/05/16.
+//  Copyright © 2016 Thibault Maekelbergh. All rights reserved.
+//
+
+import UIKit
+
+class VideosViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
