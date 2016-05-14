7d32b7dfbf52b3939a26e3ea4e4d830823a921be
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-05-14 17:36:06 +0200

    Implemented YouTube for Videos

--- xcode/Turntablism/Turntablism/VideosViewController.swift
+++ xcode/Turntablism/Turntablism/VideosViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import YouTubePlayer
 
 class VideosViewController: UIViewController {
 
+  @IBOutlet weak var video1: YouTubePlayerView!
+  @IBOutlet weak var video2: YouTubePlayerView!
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    video1.loadVideoID("rMF0pYZfuQk")
+    video2.loadVideoID("Rq1n2M4hGCw")
   }
 
 }
